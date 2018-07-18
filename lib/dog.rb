92a9f4dfb90a322bff4cbce373b2944a2780c0fe
Susan Meyers <smmeyers24@gmail.com>
2018-07-18 14:17:52 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+end
+
+  def(:name)
+   name = name
+  end
+
+    fido = Dog.new
+    fido = fido.name
+
+
+
+
+
+    #@name = name
+#end
+  #def name = name
+  #end
+#end
+
+  #fido = Dog.new
