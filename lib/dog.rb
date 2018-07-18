6fbce8223a45274ad6590f3e11b6c5cd65b9d9f5
Susan Meyers <smmeyers24@gmail.com>
2018-07-18 14:40:38 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,44 @@
+class Dog
+#setter- Writer
+ def name=(name)
+   @name = name
+ end
+ #getter - Reader
+ def name
+   @name
+ end
+
+ def breed=(breed)
+   @breed = breed
+ end
+
+ def breed
+   @breed
+ end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+    #fido = Dog.new
+    #fido = fido.name
+
+
+
+
+
+    #@name = name
+#end
+  #def name = name
+  #end
+#end
+
+  #fido = Dog.new
